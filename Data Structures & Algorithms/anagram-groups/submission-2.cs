0e0e6025aab32a52ed4264945ad43155ef9dd2ef
Durgesh Kumar <149132987+durgeshjnv2010@users.noreply.github.com>
2026-08-17 20:00:18 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        Dictionary<string, List<string>> map = new();
+        foreach(var word in strs){
+            int[] count = new int[26];
+            foreach(char c in word){
+                count[c-'a']++;
+            }
+            var key = string.Join(',', count);
+            if(!map.ContainsKey(key)){map[key] = new List<string>();}
+            map[key].Add(word);
+        }
+        return map.Values.ToList<List<string>>();
+    }
+}
