844274462ffe1b9eba4ae49c85ce0a5291057351
Durgesh Kumar <149132987+durgeshjnv2010@users.noreply.github.com>
2026-08-17 20:02:58 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) 
+    {
+        if(s.Length != t.Length)
+        {
+            return false;
+        }
+        else
+        {
+            Dictionary<char, int> map = [];
+            for (int i=0;i < s.Length; i++)
+            {
+                if (map.ContainsKey(s[i]))
+                {
+                    map[s[i]] = map[s[i]] + 1;
+                }
+                else
+                {
+                    map.Add(s[i], 1);
+                }
+                if (map.ContainsKey(t[i]))
+                {
+                    map[t[i]]--;
+                }
+                else
+                {
+                    map[t[i]] = -1;
+                }
+            }
+            foreach(var keyvalue in map)
+            {
+                map.TryGetValue(keyvalue.Key, out int value);
+                if(value != 0)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
