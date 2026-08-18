e0c006775d64fb30a473f7d5e0da3aaf19e00d11
Durgesh Kumar <149132987+durgeshjnv2010@users.noreply.github.com>
2026-08-18 14:43:56 +0530

    Update: duplicate-integer - submission-0

--- Data Structures & Algorithms/duplicate-integer/submission-0.cs
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cs
@@ -1,15 +1,20 @@
 public class Solution {
     public bool hasDuplicate(int[] nums) {
-        HashSet<int> input = new HashSet<int>();
-        
-        for(int i =0; i < nums.Length; i++){
-            if(input.Contains(nums[i])){
+
+        // Prepare Frequency Map
+        Dictionary<int, int>  count = new();
+        for(int i=0; i< nums.Length; i++){
+            if(!count.ContainsKey(nums[i])){
+                count[nums[i]] = 0;
+            }
+            count[nums[i]]++;
+        }
+
+        // Now check if el which has frq more than 1
+        foreach(var kv in count){
+            if(kv.Value > 1){
                 return true;
             }
-            else{
-                input.Add(nums[i]);
-            }
-            
         }
         return false;
     }
