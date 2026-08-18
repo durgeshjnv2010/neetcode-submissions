eaa84e949fdfdf166d19f8ec965c480874cb118a
Durgesh Kumar <149132987+durgeshjnv2010@users.noreply.github.com>
2026-08-18 16:47:00 +0530

    Update: anagram-groups - submission-1

--- Data Structures & Algorithms/anagram-groups/submission-1.cs
+++ Data Structures & Algorithms/anagram-groups/submission-1.cs
@@ -1,17 +1,33 @@
 public class Solution {
     public List<List<string>> GroupAnagrams(string[] strs) {
-        Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+        Dictionary<string, List<string>> map = new ();
 
-        foreach(string item in strs){
-            string sortedItem = new string(item.OrderBy(c => c).ToArray());
-            if(dict.ContainsKey(sortedItem)){
-                dict[sortedItem].Add(item);
+        foreach(var word in strs){
+            //char[] arr = word.ToCharArray();
+            int[] count = new int[26];
+            foreach(char c in word){
+                count[c-'a']++;
             }
-            else{
-                dict[sortedItem] = new List<string>{item};
+            
+            var key = string.Join(',',count);
+            if(!map.ContainsKey(key)){
+                map[key] = new List<string>();
             }
+            map[key].Add(word);
+
+
+
+            // Aproach 2
+
+            // Array.Sort(arr);
+
+            // var key = new String(arr);
+            // if(!map.ContainsKey(key)){
+            //     map[key] = new List<string>();
+            // }
+            // map[key].Add(word);
         }
 
-        return dict.Values.ToList();
+        return map.Values.ToList<List<string>>();
     }
 }
