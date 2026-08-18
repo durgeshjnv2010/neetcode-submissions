8150e509a193b7a089522a7fc6b32f948d792d7f
Durgesh Kumar <149132987+durgeshjnv2010@users.noreply.github.com>
2026-08-18 14:54:36 +0530

    Update: is-anagram - submission-0

--- Data Structures & Algorithms/is-anagram/submission-0.cs
+++ Data Structures & Algorithms/is-anagram/submission-0.cs
@@ -1,9 +1,31 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
-        Char[] arr1 = s.ToCharArray();
-        Char[] arr2 = t.ToCharArray();
-        Array.Sort(arr1);
-        Array.Sort(arr2);
-        return new String(arr1) == new String(arr2);
+        // First Condition , if strings of diff length then no Anagram
+        if(s.Length != t.Length){
+            return false;
+        }
+
+        int len = s.Length;
+        // Will create a freq map and for s will add and for t remove
+        // At last if any key have value > 0 then no anagram
+        Dictionary<char, int> map = new();
+        for (int i =0; i < len; i++){
+            if(!map.ContainsKey(s[i])){
+                map[s[i]] = 0;
+            }
+            map[s[i]] ++;
+        }
+        for (int i =0; i < len; i++){
+            if(map.ContainsKey(t[i])){
+                map[t[i]] --;
+            }
+        }
+
+        foreach(var kv in map){
+            if(kv.Value > 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
