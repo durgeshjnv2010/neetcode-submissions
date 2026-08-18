d35a975c71a872bc4e2e87e5b1c5774b28d11ffd
Durgesh Kumar <149132987+durgeshjnv2010@users.noreply.github.com>
2026-08-18 16:29:01 +0530

    Update: two-integer-sum - submission-0

--- Data Structures & Algorithms/two-integer-sum/submission-0.cs
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cs
@@ -1,17 +1,17 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        
-        Dictionary<int, int> dictObject = new Dictionary<int, int>();
-
-        for (int i=0; i< nums.Length; i++){
-            int diff = target- nums[i];
-
-            if(dictObject.ContainsKey(diff)){
-                return new int[2]{dictObject[diff], i};
+        /// target = x + y
+        /// y =  target - x
+        /// Keep one dict map
+        Dictionary<int, int> map = new();
+        for(int i =0; i< nums.Length; i++){
+            int second = target-nums[i];
+            
+            if(map.ContainsKey(second)){
+                return [map[second], i];
             }
-            dictObject[nums[i]] = i;
+            map[nums[i]] = i;
         }
-
-        return null;
+        return [];
     }
 }
