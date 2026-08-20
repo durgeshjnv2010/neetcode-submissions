0811ae37897c28f4d4d488fa63b544009d89e952
Durgesh Kumar <149132987+durgeshjnv2010@users.noreply.github.com>
2026-08-20 10:28:12 +0530

    Update: longest-consecutive-sequence - submission-1

--- Data Structures & Algorithms/longest-consecutive-sequence/submission-1.cs
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.cs
@@ -1,18 +1,24 @@
 public class Solution {
     public int LongestConsecutive(int[] nums) {
-        HashSet<int> set = new(nums);
+        // First lets find which is first element: Element n whose n-1 doesnt exit
+        // Then iterate from n to n+x where n+x exists in set and increase counter with each true exist
+
         int longest = 0;
-        foreach(int n in set){
-            if(!set.Contains(n-1)){
-                int current = n;
-                int length = 1;
-                while(set.Contains(current+1)){
+        HashSet<int> set = new(nums);
+        foreach(int n in nums){
+
+            if(!set.Contains(n-1)){ // First lets find which is first element: Element n whose n-1 doesnt exit
+                int current =n ;
+                int len = 1;
+                while(set.Contains(current+1)) //iterate from n to n+x where n+x exists in set and increase counter with each true exist
+                {
                     current++;
-                    length++;
+                    len++;
                 }
-                longest = Math.Max(longest, length);
+                longest = Math.Max(longest, len); // Upon completion of 1st iteration updated longest based on what len was captured
             }
         }
+
         return longest;
     }
 }
